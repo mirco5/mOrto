b9ce8206cb3ead96d6c2372a931c72e2811d9107
mirco5 <mirco.spazzoli@gmail.com>
2020-11-21 22:38:17 +0100

    Fixed csv load

--- backend/mainloop.py
+++ backend/mainloop.py
@@ -1,5 +1,5 @@
 import time
-
+import os
 from sensor import Sensor
 
 class MainLoop:
@@ -11,7 +11,8 @@ class MainLoop:
     
     def init(self):
         print("Init Started")
-        fh = open("sensorList.csv", "r")
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        fh = open(os.path.join(__location__, "sensorList.csv"), "r")
         while True:
             line = fh.readline()
             if not line:
