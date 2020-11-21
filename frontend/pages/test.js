3aa530221ea3744662ac54e6aed4a64615e4fc8f
mirco5 <mirco.spazzoli@gmail.com>
2020-11-21 22:16:22 +0100

    more

---
+++ frontend/pages/test.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { GetStaticProps } from 'next'
+
+
+export default function Test() {
+  return (<div>
+		const res = await fetch('https://192.168.1.169:5000/temperature').json()
+	</div>)
+}
